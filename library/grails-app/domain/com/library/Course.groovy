05d27dd6a9286d9a4c1f37dd1137b72c3bce5dfd
roberturry <robert_urry@live.co.uk>
2016-03-29 14:00:22 +0100

    Successfully created a project, domain class and controller class for the Course and implemented scaffolding and bootstrapping

---
+++ library/grails-app/domain/com/library/Course.groovy
@@ -0,0 +1,35 @@
+package com.library
+
+class Course {
+
+	String title
+
+	String code
+
+	String tutor
+
+	String department
+
+	String description
+
+
+	String toString(){
+
+	"$title,$code,$department"
+
+}
+
+    static constraints = {
+
+	title()
+
+	code()
+
+	tutor()
+
+	department()
+
+	description maxSize:5000
+	
+    }
+}
